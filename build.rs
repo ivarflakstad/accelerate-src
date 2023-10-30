1da60571ce185ef92d482ac1a77d673764717f94
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2023-10-30 09:59:15 +0100

    trigger ci - maybe?

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-arg=-DACCELERATE_NEW_LAPACK");
     println!("cargo:rustc-link-arg=-DACCELERATE_LAPACK_ILP64");
+    
     println!("cargo:rustc-link-lib=framework=Accelerate");
 }
